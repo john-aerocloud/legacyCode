a3f3b0bd18d3db716c34203aab4f1fc211db17f0
Qasim Ashraf <qasim.ashraf@aerocloudsystems.com>
2026-01-07 11:22:26 +0000

    Added more tests to increase coverage

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -6,6 +6,160 @@ describe('Gilded Rose', () => {
   test('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+  });
+
+  describe('Aged Brie', () => {
+    test('increases in Quality the older it gets, if the sellIn is greater than 0', () => {
+      const gildedRose = new GildedRose([new Item('Aged Brie', 2, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+      expect(items[0].sellIn).toBe(1);
+    });
+
+    test('do not increase quality if already at maximum, if the sellIn is greater than 0', () => {
+      const gildedRose = new GildedRose([new Item('Aged Brie', 2, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(1);
+    });
+
+    test('if the sellIn is 0, quality should increase by 2', () => {
+      const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(2);
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    test('if the sellIn is 0, but quality is already 50, the quality should not increase', () => {
+      const gildedRose = new GildedRose([new Item('Aged Brie', 0, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(-1);
+    });
+  });
+
+  describe('Backstage passes to a TAFKAL80ETC concert', () => {
+    test('if the sellIn is greater than 0, but sellIn less than 6, increase quality by 3', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(3);
+      expect(items[0].sellIn).toBe(4);
+    });
+
+    test('if the sellIn is greater than 5, but sellIn less than 11, increase quality by 2', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(2);
+      expect(items[0].sellIn).toBe(9);
+    });
+
+    test('if the sellIn is greater than 11, increase quality by 1', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+      expect(items[0].sellIn).toBe(11);
+    });
+
+    test('if the sellIn is greater than 11 and quality is greater than 50, then quality does not increase', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(11);
+    });
+
+    test('if the sellIn is greater than 11 and quality is less than 50, then increment quality by 1', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(11);
+    });
+
+    test('if the sellIn is greater than 11 and quality is less than 50, then increment quality', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 40)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(41);
+      expect(items[0].sellIn).toBe(11);
+    });
+
+    test('if the sellIn is 0, quality is 0, it should remain 0', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    test('if the sellIn is 0, quality is greater than 0, it should be reset to 0', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 2)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    test('do not increase quality if already at maximum, if the sellIn is greater than 0', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 2, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(1);
+    });
+  });
+
+  describe('Sulfuras, Hand of Ragnaros', () => {
+    test('quality does not change, greater than max quality', () => {
+      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(80);
+      expect(items[0].sellIn).toBe(0);
+    });
+
+    test('quality does not change, less than max quality', () => {
+      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(49);
+      expect(items[0].sellIn).toBe(0);
+    });
+
+    test('sellIn does not change', () => {
+      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 1, 1)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+      expect(items[0].sellIn).toBe(1);
+    });
+
+   test('sellIn does not change. even if less than 0', () => {
+      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -1, 1)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+      expect(items[0].sellIn).toBe(-1);
+    });
+  });
+
+  describe('Another item', () => {
+    test('if quality is 0, do not change it', () => {
+      const gildedRose = new GildedRose([new Item('Another item', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    test('if quality is greater than 0 and sellin is 0, decrement quality by 2', () => {
+      const gildedRose = new GildedRose([new Item('Another item', 0, 2)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    test('if quality is greater than 0 and sellin is greater than 0, decrement quality by 1', () => {
+      const gildedRose = new GildedRose([new Item('Another item', 1, 2)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+      expect(items[0].sellIn).toBe(0);
+    });
+  });
+
+  test('Empty array', () => {
+    const gildedRose = new GildedRose([]);
+    const items = gildedRose.updateQuality();
+    expect(items.length).toBe(0);
   });
 });
--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -19,6 +19,7 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      // If the name is not Aged Brie or Backstage or Sulfuras and the quality is greater than 0, decrease quality by 1
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -28,12 +29,16 @@ export class GildedRose {
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
+
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            // Increase quality by 1 more if sellIn < 11 and we've not reached max quality (50)
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
               }
             }
+
+            // Increase quality by 1 more if sellIn < 6 and we've not reached max quality (50)
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,9 +47,13 @@ export class GildedRose {
           }
         }
       }
+
+      // Unless it's Sulfuras, decrease sellIn by 1
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
+      // If SellIn less than 0, name is not Aged Brie or Backstage or Sulfuras and quality is greater than 0, decrease quality by 1
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -54,9 +63,10 @@ export class GildedRose {
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality = this.items[i].quality - this.items[i].quality // reset quality to 0
           }
         } else {
+          //If the name is Aged Brie, increase quality by 1 if we haven't reached max quality (50)
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
